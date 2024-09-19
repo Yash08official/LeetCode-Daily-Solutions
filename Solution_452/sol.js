f462bdd073999e4c10640e5555675bfebbb35532
Yash Wasankar <yashwasankar008@gmail.com>
2024-09-20 02:01:36 +0530

    Solution : 452. Minimum Number of Arrows to Burst Balloons

--- Solution_452/sol.js
+++ Solution_452/sol.js
@@ -5,7 +5,7 @@
 var findMinArrowShots = function(points) {
     points.sort((a, b) => a[1] - b[1]);
     let ans = 0;
-    let last = -(1 << 30);
+    let last = Number.MIN_SAFE_INTEGER;
     for (let p of points) {
         let a = p[0], b = p[1];
         if (a > last) {
